7de3da8b92affcebf36be55595d093ca309daf53
MD Rajib <83833885+blackweb71@users.noreply.github.com>
2021-11-18 12:12:03 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+x = document.querySelector("title");
+a = document.querySelector(".but1");
+b = document.querySelector(".but2");
+c = document.querySelector(".but3");
+d = document.querySelector(".but4");
+e = document.querySelector(".but5");
+
+x.innerHTML="Villain";
+a.innerHTML="Shodor Gader Er Taigar";
+b.innerHTML="Munshigiri";
+c.innerHTML="morichika";
+d.innerHTML="Joar Bhata";
+e.innerHTML="Tithir Oshukh";
